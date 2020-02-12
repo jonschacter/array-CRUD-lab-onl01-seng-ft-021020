9542bfa358a9667b566dae091cd3cceb0fca9314
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 18:44:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,3 +9,27 @@ end
 def add_element_to_end_of_array(array, element)
   array << element
 end
+
+def add_element_to_start_of_array(array, element)
+  array.unshift element
+end
+
+def remove_element_from_end_of_array(array, element)
+  array.pop element
+end
+
+def remove_element_from_start_of_array(array, element)
+  array.shift element
+end
+
+def retrieve_element_from_index(array, index)
+  array[index]
+end
+
+def retrieve_first_element_from_array(array)
+  array.first
+end
+
+def retrieve_last_element_from_array(array)
+  array.last
+end
